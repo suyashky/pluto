a85ca6025550104a81659a6623ae8d883832759a
suyash.yadav <suyash.yadav@bottomline.com>
2023-04-21 11:45:36 +0100

    feat: Initial commit

---
+++ src/test/java/com/planet/pluto/PlutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planet.pluto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
